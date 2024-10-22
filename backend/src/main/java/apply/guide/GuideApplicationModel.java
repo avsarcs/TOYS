9526bf70ea026252c6bc518ed8b8fdf0715d19c3
Ruşen Ali Yılmaz <36644037+LikeGamerTr@users.noreply.github.com>
2024-10-22 12:21:04 +0300

    [update] GuideApplicationModel updated according to email response.

--- backend/src/main/java/apply/guide/GuideApplicationModel.java
+++ backend/src/main/java/apply/guide/GuideApplicationModel.java
@@ -2,12 +2,17 @@ package apply.guide;
 
 import enums.DEPARTMENT;
 
+import java.time.ZonedDateTime;
+
 public class GuideApplicationModel {
     private String full_name;
     private String bilkent_id;
     private String email;
-    private String phoneNumbers;
+    private String phoneNumber;
     private DEPARTMENT department;
+    private ZonedDateTime birth_date;
+    private String IBAN;
+    private String address;
     private int current_semester;
     private boolean next_semester_exchange;
     private String howdidyouhear;
