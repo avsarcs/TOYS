68278807d749b98611f2f9fbcaf4e0e530bc227e
Mehmet Emin Avşar <53301542+avsarcs@users.noreply.github.com>
2024-12-14 15:43:20 +0300

    Revision data model (#18)
    * [data] Added Schedule Model and added reviews to the User Model
    
    * Added recipient name to Advisor Offer Model
    
    * Reviews no longer need to be approved by the coordinator, but they can be deleted
    
    * A review can be only for a single guide, not multiple guides. Fixed mistake
    
    * Created Review Details Page
    
    ---------
    
    Co-authored-by: steins-ut <51322187+steins-ut@users.noreply.github.com>

--- toys-frontend/src/App.tsx
+++ toys-frontend/src/App.tsx
@@ -21,6 +21,7 @@ import Dashboard from "./pages/Dashboard/Dashboard.tsx";
 import EditGuideProfilePage from "./pages/Guide/EditGuideProfilePage.tsx";
 import TourPage from "./pages/TourInformation/TourPage.tsx";
 import TourReviewPage from "./pages/TourReview/TourReview.tsx";
+import ReviewDetailsPage from "./pages/ReviewDetails/ReviewDetails.tsx";
 
 import "dayjs/locale/tr"
 import dayjs from "dayjs";
@@ -61,6 +62,7 @@ function App() {
             <Route path="/profile" element={<GuideProfilePage />}/>
             <Route path="/edit-profile" element={<EditGuideProfilePage />}/>
             <Route path="/review-tour/:reviewer-id" element={<TourReviewPage />}/>
+            <Route path="/review/:review-id" element={<ReviewDetailsPage />}/>
             <Route path="/tourstatistics" element={<TourStatistics />}/>
           </Routes>
         </main>
