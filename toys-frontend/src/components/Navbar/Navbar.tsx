aa3bb54a7192cf848270e349e8ae69149dce3ddd
Begüm Filiz Öz <begumflzoz@gmail.com>
2024-12-18 00:55:28 +0300

    Navbar updated

--- toys-frontend/src/components/Navbar/Navbar.tsx
+++ toys-frontend/src/components/Navbar/Navbar.tsx
@@ -1,20 +1,26 @@
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Group, Code, ScrollArea, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import {
-  IconNotes,
-  IconCalendarStats,
   IconGauge,
   IconPresentationAnalytics,
   IconFileAnalytics,
-  IconAdjustments,
   IconLock,
+  IconUsers,
+  IconUser,
+  IconLogout,
+  IconMap,
+  IconBuildingCircus,
+  IconUserUp,
+  IconBrandSafari,
+  IconCreditCard,
+  IconLayoutDashboard
+
 } from '@tabler/icons-react';
 import { UserButton } from '../UserButton/UserButton';
-import { LinksGroup } from './NavbarLinksGroup';
-import { useDisclosure } from "@mantine/hooks";
-import classes from './Navbar.module.css';
 import { UserContext } from '../../context/UserContext';
-import { Link, To, useNavigate } from 'react-router-dom';
-import { JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useContext } from 'react';
+import classes from './Navbar.module.css';
 import { UserRole } from '../../types/enum';
 
 type LinkType = {
@@ -31,98 +37,119 @@ export const Navbar: React.FC = () => {
   const [opened, { toggle }] = useDisclosure(false);
   const navigate = useNavigate();
 
+  const [activeMenu, setActiveMenu] = useState<string | null>(null);
+
   const handleLogout = () => {
     userContext.setAuthToken('');
-    navigate('/login'); // Redirect to login page after logout
+    navigate('/login');
   };
 
   const links: LinkType[] = [
-    { label: 'Profile', icon: IconGauge, link: '/profile' },
-    { label: 'Logout', icon: IconGauge, onClick: handleLogout },
-    { label: 'All Tours', icon: IconGauge, link: '/tours' },
+    { label: 'Profil', icon: IconUser, link: '/profile' },
+    { label: 'Çıkış Yapın', icon: IconLogout, onClick: handleLogout },
+    { label: 'Tüm Turlar', icon: IconMap, link: '/tours' },
   ];
 
-  // Add conditional links for DIRECTOR role
   if (user.role === UserRole.DIRECTOR) {
-    //console.log(user.role);
+    links.push({ label: 'Tüm Fuarlar', icon: IconBuildingCircus, link: '#' });
+    links.push({ label: 'Rehber Başvuruları', icon: IconBrandSafari, link: '/toys-applications' });
+    links.push({ label: 'Danışman Teklifleri', icon: IconUserUp, link: '/advisor-offers' });
+    links.push({ label: 'Ödemeler', icon: IconCreditCard, link: '/guide-payments' });
+    links.push({ label: 'Personel Yönetimi', icon: IconUsers, link: '#' });
     links.push({
-      label: 'Analytics',
+      label: 'Veri Analizi',
       icon: IconPresentationAnalytics,
       subLinks: [
-        { label: 'Universities', link: '/universitieslist' },
-        { label: 'Rivals', link: '/rivalslist' },
-        { label: 'High Schools', link: '/highschoolslist' },
-        { label: 'Bilkent Students', link: '/bilkentstudentdetails' },
-        { label: 'Tour Statistics', link: '/tourstatistics' },
+        { label: 'Üniversiteler Listesi', link: '/universitieslist' },
+        { label: 'Rakip Üniversiteler', link: '/rivalslist' },
+        { label: 'Üniversite Karşılaştırma', link: '/comparison' },
+        { label: 'Liseler Listesi', link: '/highschoolslist' },
+        { label: 'Bilkent Öğrenci Verisi', link: '/bilkentstudentdetails' },
+        { label: 'Tur İstatistikleri', link: '/tourstatistics' },
       ],
     });
-    links.push({ label: 'All Fairs', icon: IconFileAnalytics, link: '#' });
-    links.push({ label: 'Guide Applications', icon: IconGauge, link: '/toys-applications' });
   }
-  else if (user.role === UserRole.COORDINATOR ) {
-    links.push({ label: 'Admin Panel', icon: IconLock, link: '' });
+  else if (user.role === UserRole.COORDINATOR) {
+    links.push({ label: 'Tüm Fuarlar', icon: IconBuildingCircus, link: '#' });
+    links.push({ label: 'Rehber Başvuruları', icon: IconBrandSafari, link: '/toys-applications' });
+    links.push({ label: 'Danışman Teklifleri', icon: IconUserUp, link: '/advisor-offers' });
+    links.push({ label: 'Ödemeler', icon: IconCreditCard, link: '/guide-payments' });
+    links.push({ label: 'Personel Yönetimi', icon: IconUsers, link: '#' });
   }
-  else if (user.role === UserRole.ADVISOR ) {
-    links.push({ label: 'Admin Panel', icon: IconLock, link: '' });
+  else if (user.role === UserRole.ADVISOR) {
+    links.push({ label: 'Pano', icon: IconLayoutDashboard, link: '/dashboard' });
+    links.push({ label: 'Rehberler', icon: IconUsers, link: '#' });
   }
-  else if (user.role === UserRole.GUIDE ) {
-    links.push({ label: 'Admin Panel', icon: IconLock, link: '' });
+  else if (user.role === UserRole.GUIDE) {
+    links.push({ label: 'Pano', icon: IconLayoutDashboard, link: '/dashboard' });
   }
-  else if (user.role === UserRole.TRAINEE ) {
-    links.push({ label: 'Admin Panel', icon: IconLock, link: '' });
+  else if (user.role === UserRole.TRAINEE) {
+    links.push({ label: 'Pano', icon: IconLayoutDashboard, link: '/dashboard' });
   }
-  console.log(user.role);
+
+  const toggleSubMenu = (label: string) => {
+    setActiveMenu(activeMenu === label ? null : label);
+  };
 
   return (
-    <nav className={`${opened ? (classes.navbar + " " + classes.open) : classes.navbar}`}>
+    <nav className={`${opened ? `${classes.navbar} ${classes.open}` : classes.navbar}`}>
       <div className={classes.header}>
         <Group justify="space-between">
-          {opened ? <Code fw={700}>TOYS</Code> : <></>}
-          <Burger size="md" opened={opened} onClick={toggle}/>
+          {opened && <Code fw={700}>TOYS</Code>}
+          <Burger size="md" opened={opened} onClick={toggle} />
         </Group>
       </div>
 
       <ScrollArea className={classes.links}>
-      <div className={classes.linksInner}>
-        {opened ? (
-          links.map((link, index) => (
-            <div key={index}>
-              {/* Main Link */}
-              {link.link ? (
-                <Link to={link.link} className={classes.link}>
+        <div className={classes.linksInner}>
+          {opened &&
+            links.map((link, index) => (
+              <div key={index} className={classes.linkWrapper}>
+                {/* Main Link */}
+                {link.link ? (
+                  <Link to={link.link} className={classes.link}>
+                    <link.icon size={20} />
+                    {link.label}
+                  </Link>
+                ) : (
+                  <button
+                  onClick={() => (link.subLinks ? toggleSubMenu(link.label) : link.onClick?.())}
+                  className={classes.link}
+                >
                   <link.icon size={20} />
                   {link.label}
-                </Link>
-              ) : (
-                <button onClick={link.onClick} className={classes.link}>
-                  <link.icon size={20} />
-                  {link.label}
+
+                  {/* Add the arrow for parent links with sub-links */}
+                  {link.subLinks && (
+                    <span
+                      className={`${classes.arrow} ${
+                        activeMenu === link.label ? classes.arrowOpen : ""
+                      }`}
+                    >
+                      &#9662; {/* Unicode down arrow */}
+                    </span>
+                  )}
                 </button>
-              )}
+                )}
 
-              {/* Render subLinks if they exist */}
-              {link.subLinks && (
-                <div className={classes.subLinks}>
-                  {link.subLinks.map((subLink: { link: To; label: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }, subIndex: Key | null | undefined) => (
-                    <Link key={subIndex} to={subLink.link} className={classes.sublink}>
-                      {subLink.label}
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </div>
-          ))
-        ) : (
-          <></>
-        )}
-      </div>
-    </ScrollArea>
+                {/* Sub-links */}
+                {link.subLinks && (
+                  <ul className={`${classes.subLinksWrapper} ${activeMenu === link.label ? classes.open : ''}`}>
+                    {link.subLinks.map((subLink, subIndex) => (
+                      <li key={subIndex} className={classes.subLinkItem}>
+                        <Link to={subLink.link} className={classes.subLink}>
+                          {subLink.label}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+        </div>
+      </ScrollArea>
 
-
-      <div className={classes.footer}>
-        {opened ? <UserButton/> : <></>}
-      </div>
-
+      <div className={classes.footer}>{opened && <UserButton />}</div>
     </nav>
   );
-}
+};
