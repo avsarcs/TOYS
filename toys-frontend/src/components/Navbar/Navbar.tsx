a0b0147c449fa7c60966abcd50ae33a06c3fafb3
Begüm Filiz Öz <113853370+begumfilizoz@users.noreply.github.com>
2024-12-18 22:10:47 +0300

    Feat navbar (#20)
    * navbar updated
    
    * Navbar updated
    
    * navbar updated
    
    * navbar userbutton updated to include picture, name, email
    
    * Navbar updated: minor changes like logo added

--- toys-frontend/src/components/Navbar/Navbar.tsx
+++ toys-frontend/src/components/Navbar/Navbar.tsx
@@ -1,84 +1,165 @@
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Group, Code, ScrollArea, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import {
-  IconNotes,
-  IconCalendarStats,
   IconGauge,
   IconPresentationAnalytics,
   IconFileAnalytics,
-  IconAdjustments,
   IconLock,
+  IconUsers,
+  IconUser,
+  IconLogout,
+  IconMap,
+  IconBuildingCircus,
+  IconUserUp,
+  IconBrandSafari,
+  IconCreditCard,
+  IconLayoutDashboard
+
 } from '@tabler/icons-react';
 import { UserButton } from '../UserButton/UserButton';
-import { LinksGroup } from './NavbarLinksGroup';
-import { useDisclosure } from "@mantine/hooks";
+import { UserContext } from '../../context/UserContext';
 import classes from './Navbar.module.css';
+import { UserRole } from '../../types/enum';
 
-const mockdata = [
-  { label: 'Dashboard', icon: IconGauge },
-  {
-    label: 'Market news',
-    icon: IconNotes,
-    initiallyOpened: true,
-    links: [
-      { label: 'Overview', link: '/' },
-      { label: 'Forecasts', link: '/' },
-      { label: 'Outlook', link: '/' },
-      { label: 'Real time', link: '/' },
-    ],
-  },
-  {
-    label: 'Releases',
-    icon: IconCalendarStats,
-    links: [
-      { label: 'Upcoming releases', link: '/' },
-      { label: 'Previous releases', link: '/' },
-      { label: 'Releases schedule', link: '/' },
-    ],
-  },
-  {
-    label: 'Üniversite Analizi',
-    icon: IconPresentationAnalytics,
-    links: [
-      { label: 'Tüm Üniversiteler', link: '/universitieslist' },
-      { label: 'Rakip Üniversiteler', link: '/' },
-      { label: 'Üniversite Karşılaştırma', link: '/comparison' },
-    ],
-  },
-  { label: 'Contracts', icon: IconFileAnalytics },
-  { label: 'Settings', icon: IconAdjustments },
-  {
-    label: 'Security',
-    icon: IconLock,
-    links: [
-      { label: 'Enable 2FA', link: '/' },
-      { label: 'Change password', link: '/' },
-      { label: 'Recovery codes', link: '/' },
-    ],
-  },
-];
+type LinkType = {
+  label: string;
+  icon: React.ComponentType<Icon['props']>;
+  link?: string;
+  onClick?: () => void;
+  subLinks?: { label: string; link: string }[];
+};
 
-export function Navbar() {
+export const Navbar: React.FC = () => {
+  const { user } = useContext(UserContext);
+  const userContext = useContext(UserContext);
   const [opened, { toggle }] = useDisclosure(false);
+  const navigate = useNavigate();
 
-  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+  const [activeMenu, setActiveMenu] = useState<string | null>(null);
+
+  const handleLogout = () => {
+    userContext.setAuthToken('');
+    navigate('/login');
+  };
+
+  const links: LinkType[] = [
+    { label: 'Profil', icon: IconUser, link: '/profile' },
+    { label: 'Çıkış Yapın', icon: IconLogout, onClick: handleLogout },
+    { label: 'Tüm Turlar', icon: IconMap, link: '/tours' },
+  ];
+
+  if (user.role === UserRole.DIRECTOR) {
+    links.push({ label: 'Tüm Fuarlar', icon: IconBuildingCircus, link: '#' });
+    links.push({ label: 'Rehber Başvuruları', icon: IconBrandSafari, link: '/toys-applications' });
+    links.push({ label: 'Danışman Teklifleri', icon: IconUserUp, link: '/advisor-offers' });
+    links.push({ label: 'Ödemeler', icon: IconCreditCard, link: '/guide-payments' });
+    links.push({ label: 'Personel Yönetimi', icon: IconUsers, link: '#' });
+    links.push({
+      label: 'Veri Analizi',
+      icon: IconPresentationAnalytics,
+      subLinks: [
+        { label: 'Üniversiteler Listesi', link: '/universitieslist' },
+        { label: 'Rakip Üniversiteler', link: '/rivalslist' },
+        { label: 'Üniversite Karşılaştırma', link: '/comparison' },
+        { label: 'Liseler Listesi', link: '/highschoolslist' },
+        { label: 'Bilkent Öğrenci Verisi', link: '/bilkentstudentdetails' },
+        { label: 'Tur İstatistikleri', link: '/tourstatistics' },
+      ],
+    });
+  }
+  else if (user.role === UserRole.COORDINATOR) {
+    links.push({ label: 'Tüm Fuarlar', icon: IconBuildingCircus, link: '#' });
+    links.push({ label: 'Rehber Başvuruları', icon: IconBrandSafari, link: '/toys-applications' });
+    links.push({ label: 'Danışman Teklifleri', icon: IconUserUp, link: '/advisor-offers' });
+    links.push({ label: 'Ödemeler', icon: IconCreditCard, link: '/guide-payments' });
+    links.push({ label: 'Personel Yönetimi', icon: IconUsers, link: '#' });
+  }
+  else if (user.role === UserRole.ADVISOR) {
+    links.push({ label: 'Pano', icon: IconLayoutDashboard, link: '/dashboard' });
+    links.push({ label: 'Rehberler', icon: IconUsers, link: '#' });
+  }
+  else if (user.role === UserRole.GUIDE) {
+    links.push({ label: 'Pano', icon: IconLayoutDashboard, link: '/dashboard' });
+  }
+  else if (user.role === UserRole.TRAINEE) {
+    links.push({ label: 'Pano', icon: IconLayoutDashboard, link: '/dashboard' });
+  }
+
+  const toggleSubMenu = (label: string) => {
+    setActiveMenu(activeMenu === label ? null : label);
+  };
 
   return (
-    <nav className={`${opened ? (classes.navbar + " " + classes.open) : classes.navbar}`}>
+    <nav className={`${opened ? `${classes.navbar} ${classes.open}` : classes.navbar}`}>
       <div className={classes.header}>
         <Group justify="space-between">
-          {opened ? <Code fw={700}>TOYS</Code> : <></>}
-          <Burger size="md" opened={opened} onClick={toggle}/>
+          {opened && (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <img src="/bilkent-tr-amblem.png" alt="Logo" style={{ marginRight: '10px', height: '40px' }} />
+              <Code fw={700} style={{ fontSize: '24px' }}>TOYS</Code>
+            </div>
+          )}
+          <Burger size="md" opened={opened} onClick={toggle} />
         </Group>
+        <div className={classes.divider} />
       </div>
-
+      <div className={classes.header} style={{ marginTop: '0', paddingTop: '20px', backgroundColor: '#e0f7ff' }}>
+        {opened && <UserButton />}
+        <div className={classes.divider} style={{ marginTop: '10px' }} />
+      </div>
+     
       <ScrollArea className={classes.links}>
-        <div className={classes.linksInner}>{opened ? links : <></>}</div>
+        <div className={classes.linksInner}>
+          {opened &&
+            links.map((link, index) => (
+              <div key={index} className={classes.linkWrapper}>
+                {/* Main Link */}
+                {link.link ? (
+                  <Link to={link.link} className={classes.link}>
+                    <link.icon size={20} />
+                    {link.label}
+                  </Link>
+                ) : (
+                  <button
+                  onClick={() => (link.subLinks ? toggleSubMenu(link.label) : link.onClick?.())}
+                  className={classes.link}
+                >
+                  <link.icon size={20} />
+                  {link.label}
+
+                  {/* Add the arrow for parent links with sub-links */}
+                  {link.subLinks && (
+                    <span
+                      className={`${classes.arrow} ${
+                        activeMenu === link.label ? classes.arrowOpen : ""
+                      }`}
+                    >
+                      &#9662; {/* Unicode down arrow */}
+                    </span>
+                  )}
+                </button>
+                )}
+
+                {/* Sub-links */}
+                {link.subLinks && (
+                  <ul className={`${classes.subLinksWrapper} ${activeMenu === link.label ? classes.open : ''}`}>
+                    {link.subLinks.map((subLink, subIndex) => (
+                      <li key={subIndex} className={classes.subLinkItem}>
+                        <Link to={subLink.link} className={classes.subLink}>
+                          {subLink.label}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+        </div>
       </ScrollArea>
 
-      <div className={classes.footer}>
-        {opened ? <UserButton/> : <></>}
-      </div>
-
+      
     </nav>
   );
-}
+};
--- toys-frontend/src/components/UserButton/UserButton.tsx
+++ toys-frontend/src/components/UserButton/UserButton.tsx
@@ -1,27 +1,36 @@
 import { UnstyledButton, Group, Avatar, Text, rem } from '@mantine/core';
 import { IconChevronRight } from '@tabler/icons-react';
 import classes from './UserButton.module.css';
+import { UserContext } from '../../context/UserContext';
+import React, { useContext, useState } from 'react';
 
 export function UserButton() {
+  const { user } = useContext(UserContext);
+  const userContext = useContext(UserContext);
   return (
     <UnstyledButton className={classes.user}>
       <Group>
         <Avatar
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+          src={userContext.user.profile.profile_picture}
           radius="xl"
         />
 
         <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
-            Harriette Spoonlicker
-          </Text>
+        <Text size="sm" fw={500}>
+        <b>{userContext.user.profile.fullname}</b>
+        </Text>
 
-          <Text c="dimmed" size="xs">
-            hspoonlicker@outlook.com
+        <Text size="xs">
+          {userContext.user.profile.role}
           </Text>
+          <Text c="dimmed" size="xs">
+          {/*{userContext.user.profile.email}*/}
+          email
+          </Text>
+          
         </div>
 
-        <IconChevronRight style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
+        
       </Group>
     </UnstyledButton>
   );
