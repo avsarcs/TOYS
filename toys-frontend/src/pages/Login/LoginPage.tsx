722abb8503e8911a65298e9605ee4d45632ec6e1
steinsut <51322187+steins-ut@users.noreply.github.com>
2024-12-22 21:15:17 +0300

    feat: add default home pages for different user roles

--- toys-frontend/src/pages/Login/LoginPage.tsx
+++ toys-frontend/src/pages/Login/LoginPage.tsx
@@ -1,9 +1,10 @@
-import { Image } from "@mantine/core"
+import {Image} from "@mantine/core"
 import LoginForm from "../../components/Login/LoginForm";
 import {useContext, useEffect, useState} from "react";
 import RegisterForm from "../../components/Login/RegisterForm.tsx";
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {UserContext} from "../../context/UserContext.tsx";
+import {UserRole} from "../../types/enum.ts";
 
 const LoginPage: React.FC = () => {
   const [searchParams] = useSearchParams();
@@ -13,9 +14,23 @@ const LoginPage: React.FC = () => {
 
   useEffect(() => {
     if(userContext.isLoggedIn) {
-      navigate(searchParams.get("redirect") ?? "/dashboard", { replace: true });
+      if(searchParams.has("redirect")) {
+        navigate(searchParams.get("redirect") as string);
+      }
+      else {
+        switch(userContext.user.role) {
+          case UserRole.COORDINATOR:
+            navigate("/tours");
+            break;
+          case UserRole.DIRECTOR:
+            navigate("/universitieslist");
+            break;
+          default:
+            navigate("/dashboard");
+        }
+      }
     }
-  }, [userContext.isLoggedIn]);
+  }, [userContext.user.role]);
 
   return (
     <div className={"w-full min-h-screen grid lg:grid-cols-2 grid-cols-1 lg:grid-rows-1 grid-rows-2"}>
