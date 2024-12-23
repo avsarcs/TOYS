69a17cab51bfd637b2556f5e2627e57b587dc257
Begüm Filiz Öz <begumflzoz@gmail.com>
2024-12-23 19:21:40 +0300

    Tours list item updated to include time and date

--- toys-frontend/src/components/TourList/ListItem.tsx
+++ toys-frontend/src/components/TourList/ListItem.tsx
@@ -3,6 +3,7 @@ import {TourListItemProps} from "../../types/designed.ts";
 import {Link} from "react-router-dom";
 import React from "react";
 import {TourStatusText} from "../../types/enum.ts";
+import { IconCalendarWeek } from "@tabler/icons-react";
 
 // Map status to appropriate semantic colors
 const statusColorMap = {
@@ -15,16 +16,39 @@ const statusColorMap = {
   FINISHED: "purple"
 } as const;
 
+
 const ListItem: React.FC<TourListItemProps> = (props: TourListItemProps) => {
   // Get color for current status, defaulting to black if status not found
   const statusColor = statusColorMap[props.tour.event_status as keyof typeof statusColorMap] || "black";
-  
+  const formatISODate = (isoDate: string | number | Date) => {
+    const date = new Date(isoDate);
+
+    // Convert to time zone-adjusted string
+    return date.toLocaleString("en-GB", {
+        timeZone: "UTC", // Adjust time to your desired time zone (e.g., "Europe/London", "America/New_York")
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hourCycle: "h23", // Use 24-hour time format
+    });
+};
+
   return (
     <Paper withBorder shadow="xs" radius="0" className="bg-gray-100" component={Link} to={`/tour/${props.tour.event_id}`}>
       <Box p="0" className="transition-transform hover:translate-x-1.5 hover:cursor-pointer">
         <Flex direction="row">
           <Box className="flex-grow flex-shrink basis-1/2 bg-gray-100" p="lg">
-            {props.tour.highschool.name}
+            <b>{props.tour.highschool.name}</b>
+            <Box mt="sm">
+            {props.tour.accepted_time && (
+              <Flex align="center">
+              <IconCalendarWeek size={16} />
+              <Text ml="xs">{formatISODate(props.tour.accepted_time)}</Text>
+              </Flex>
+            )}
+            </Box>
           </Box>
           <Center className="flex-grow flex-shrink basis-1/4 bg-gray-50" p="lg">
             {props.tour.visitor_count} Kişi
