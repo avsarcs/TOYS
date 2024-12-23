7ad0cf474a3ba5cf86e1a9c6440f8a58e3138be2
Begüm Filiz Öz <begumflzoz@gmail.com>
2024-12-23 03:30:25 +0300

    bug fixes

--- toys-frontend/src/components/FairInformation/ApplicantInformation.tsx
+++ toys-frontend/src/components/FairInformation/ApplicantInformation.tsx
@@ -8,7 +8,7 @@ const ApplicantInformation: React.FC<FairSectionProps> = (props: FairSectionProp
     <Box p="lg">
       <Group>
         <Text size="md" fw={700}>Başvuran Bilgileri: &nbsp;
-          <Text span>{ props.fair.applicant.fullname }</Text>
+          <Text span>{ props.fair.applicant ? props.fair.applicant.fullname : "N/A" }</Text>
         </Text>
         <Popover trapFocus>
           <Popover.Target>
@@ -16,14 +16,14 @@ const ApplicantInformation: React.FC<FairSectionProps> = (props: FairSectionProp
           </Popover.Target>
           <Popover.Dropdown p="lg" className="shadow-lg">
             <Text size="md" fw={700}>
-              Email: &nbsp;<Text span>{ props.fair.applicant.email }</Text> &nbsp;
+              Email: &nbsp;<Text span>{ props.fair.applicant ? props.fair.applicant.email : "N/A"}</Text> &nbsp;
               <Button><Text span className="iconify text-lg solar--clipboard-list-linear"/></Button>
             </Text>
             <Space h="sm"/>
             <Text size="md" fw={700}>
               Telefon No: &nbsp;
               <Text span>
-                { parsePhoneNumber(props.fair.applicant.phone, "TR").formatInternational() }
+                { parsePhoneNumber(props.fair.applicant ? props.fair.applicant.phone : "0000000000", "TR").formatInternational() }
               </Text> &nbsp;
               <Button><Text span className="iconify text-lg solar--clipboard-list-linear"/></Button>
             </Text>
